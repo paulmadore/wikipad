09e40d874138093699dd1b1cdc348dd71b2bec08
moonpunter@gmail.com <henrypaulmadore@gmail.com>
2016-01-14 15:03:39 -0600

    -

---
+++ ext/gtksourceview/tests/test-buffer.c
@@ -0,0 +1,237 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; coding: utf-8 -*- */
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <gtksourceview/gtksource.h>
+
+static const char *c_snippet =
+	"#include <foo.h>\n"
+	"\n"
+	"/* this is a comment */\n"
+	"int main() {\n"
+	"}\n";
+
+/* If we are running from the source dir (e.g. during make check)
+ * we override the path to read from the data dir
+ */
+static void
+init_default_manager (void)
+{
+	gchar *dir;
+
+	dir = g_build_filename (TOP_SRCDIR, "data", "language-specs", NULL);
+
+	if (g_file_test (dir, G_FILE_TEST_IS_DIR))
+	{
+		GtkSourceLanguageManager *lm;
+		gchar **lang_dirs;
+
+		lm = gtk_source_language_manager_get_default ();
+
+		lang_dirs = g_new0 (gchar *, 2);
+		lang_dirs[0] = dir;
+
+		gtk_source_language_manager_set_search_path (lm, lang_dirs);
+		g_strfreev (lang_dirs);
+	}
+	else
+	{
+		g_free (dir);
+	}
+}
+
+static void
+test_get_buffer (void)
+{
+	GtkWidget *view;
+	GtkSourceBuffer *buffer;
+
+	view = gtk_source_view_new ();
+
+	buffer = GTK_SOURCE_BUFFER (gtk_text_view_get_buffer (GTK_TEXT_VIEW (view)));
+
+	g_assert (buffer != NULL);
+	g_assert (GTK_SOURCE_IS_BUFFER (buffer));
+
+	if (g_object_is_floating (view))
+	{
+		g_object_ref_sink (view);
+	}
+
+	/* Here we check if notify_buffer recreates the buffer while view is being
+	 * destroyed, which causes assertion failure in GtkTextView's finalize ()
+	 * function.
+	 * Please see: https://bugzilla.gnome.org/show_bug.cgi?id=634510 */
+	g_object_unref (view);
+}
+
+static void
+test_get_context_classes (void)
+{
+	GtkSourceLanguageManager *lm;
+	GtkSourceBuffer *buffer;
+	GtkSourceLanguage *lang;
+	GtkTextIter start, end, i;
+	char **classes;
+
+	/* test plain text */
+	buffer = gtk_source_buffer_new (NULL);
+	gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), "some text", -1);
+	gtk_text_buffer_get_bounds (GTK_TEXT_BUFFER (buffer), &start, &end);
+	gtk_source_buffer_ensure_highlight (buffer, &start, &end);
+
+	gtk_text_buffer_get_start_iter (GTK_TEXT_BUFFER (buffer), &i);
+	classes = gtk_source_buffer_get_context_classes_at_iter (buffer, &i);
+	g_assert_cmpuint (g_strv_length (classes), ==, 0);
+	g_strfreev (classes);
+
+	g_object_unref (buffer);
+
+	/* test C */
+	lm = gtk_source_language_manager_get_default ();
+	lang = gtk_source_language_manager_get_language (lm, "c");
+	g_assert (GTK_SOURCE_IS_LANGUAGE (lang));
+	buffer = gtk_source_buffer_new_with_language (lang);
+	gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), c_snippet, -1);
+	gtk_text_buffer_get_bounds (GTK_TEXT_BUFFER (buffer), &start, &end);
+	gtk_source_buffer_ensure_highlight (buffer, &start, &end);
+
+	gtk_text_buffer_get_start_iter (GTK_TEXT_BUFFER (buffer), &i);
+	classes = gtk_source_buffer_get_context_classes_at_iter (buffer, &i);
+	g_assert_cmpuint (g_strv_length (classes), ==, 1);
+	g_assert_cmpstr (classes[0], ==, "no-spell-check");
+	g_strfreev (classes);
+
+	gtk_text_buffer_get_iter_at_line_offset (GTK_TEXT_BUFFER (buffer), &i, 2, 5);
+	classes = gtk_source_buffer_get_context_classes_at_iter (buffer, &i);
+	g_assert_cmpuint (g_strv_length (classes), ==, 1);
+	g_assert_cmpstr (classes[0], ==, "comment");
+	g_strfreev (classes);
+
+	g_object_unref (buffer);
+}
+
+static void
+do_test_change_case (GtkSourceBuffer         *buffer,
+		     GtkSourceChangeCaseType  case_type,
+		     const gchar             *text,
+		     const gchar             *expected)
+{
+	GtkTextIter start;
+	GtkTextIter end;
+	gchar *changed;
+	gchar *changed_normalized;
+	gchar *expected_normalized;
+
+	gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), text, -1);
+
+	gtk_text_buffer_get_bounds (GTK_TEXT_BUFFER (buffer), &start, &end);
+	gtk_source_buffer_change_case (buffer, case_type, &start, &end);
+
+	gtk_text_buffer_get_bounds (GTK_TEXT_BUFFER (buffer), &start, &end);
+	changed = gtk_text_buffer_get_text (GTK_TEXT_BUFFER (buffer), &start, &end, TRUE);
+
+	changed_normalized = g_utf8_normalize (changed, -1, G_NORMALIZE_DEFAULT);
+	expected_normalized = g_utf8_normalize (expected, -1, G_NORMALIZE_DEFAULT);
+
+	g_assert_cmpstr (changed_normalized, ==, expected_normalized);
+
+	g_free (changed);
+	g_free (changed_normalized);
+	g_free (expected_normalized);
+}
+
+static void
+test_change_case (void)
+{
+	GtkSourceBuffer *buffer;
+
+	buffer = gtk_source_buffer_new (NULL);
+
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_LOWER, "some TEXT", "some text");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_UPPER, "some TEXT", "SOME TEXT");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TOGGLE, "some TEXT", "SOME text");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TITLE, "some TEXT", "Some Text");
+
+	/* https://bugzilla.gnome.org/show_bug.cgi?id=416390 */
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_LOWER, "T̈OME", "ẗome");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_UPPER, "ẗome", "T̈OME");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TOGGLE, "ẗome", "T̈OME");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TOGGLE, "T̈OME", "ẗome");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TITLE, "ẗome", "T̈ome");
+
+	/* test g_unichar_totitle */
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_LOWER, "\307\261adzíki", "\307\263adzíki");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_LOWER, "\307\262adzíki", "\307\263adzíki");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_LOWER, "\307\263adzíki", "\307\263adzíki");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_UPPER, "\307\263adzíki", "\307\261ADZÍKI");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_UPPER, "\307\262adzíki", "\307\261ADZÍKI");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TOGGLE, "\307\263adzíki", "\307\261ADZÍKI");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TITLE, "\307\263adzíki", "\307\262adzíki");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TITLE, "\307\261ADZÍKI", "\307\262adzíki");
+	do_test_change_case (buffer, GTK_SOURCE_CHANGE_CASE_TITLE, "\307\262ADZÍKI", "\307\262adzíki");
+
+	g_object_unref (buffer);
+}
+
+static void
+do_test_join_lines (GtkSourceBuffer *buffer,
+		    const gchar     *text,
+		    const gchar     *expected,
+		    gint             start_offset,
+		    gint             end_offset)
+{
+	GtkTextIter start;
+	GtkTextIter end;
+	gchar *changed;
+
+	gtk_text_buffer_set_text (GTK_TEXT_BUFFER (buffer), text, -1);
+
+	gtk_text_buffer_get_iter_at_offset (GTK_TEXT_BUFFER (buffer), &start, start_offset);
+	gtk_text_buffer_get_iter_at_offset (GTK_TEXT_BUFFER (buffer), &end, end_offset);
+
+	gtk_source_buffer_join_lines (buffer, &start, &end);
+
+	gtk_text_buffer_get_bounds (GTK_TEXT_BUFFER (buffer), &start, &end);
+	changed = gtk_text_buffer_get_text (GTK_TEXT_BUFFER (buffer), &start, &end, TRUE);
+
+	g_assert_cmpstr (changed, ==, expected);
+
+	g_free (changed);
+}
+
+static void
+test_join_lines (void)
+{
+	GtkSourceBuffer *buffer;
+
+	buffer = gtk_source_buffer_new (NULL);
+
+	do_test_join_lines (buffer, "some text", "some text", 0, -1);
+	do_test_join_lines (buffer, "some\ntext", "some text", 0, -1);
+	do_test_join_lines (buffer, "some\t  \n\t  text", "some text", 0, -1);
+	do_test_join_lines (buffer, "some\n\n\ntext", "some text", 0, -1);
+	do_test_join_lines (buffer, "some\ntext", "some\ntext", 0, 1);
+	do_test_join_lines (buffer, "some\ntext", "some\ntext", 6, -1);
+	do_test_join_lines (buffer, "some\ntext\nmore", "some text\nmore", 0, 6);
+	do_test_join_lines (buffer, "some\ntext\nmore", "some\ntext more", 6, -1);
+	do_test_join_lines (buffer, "some\n   text\nmore", "some text\nmore", 0, 5);
+	do_test_join_lines (buffer, "some\ntext\n\n\nmore", "some text\n\nmore", 0, 10);
+
+	g_object_unref (buffer);
+}
+
+int
+main (int argc, char** argv)
+{
+	gtk_test_init (&argc, &argv);
+
+	init_default_manager ();
+
+	g_test_add_func ("/Buffer/bug-634510", test_get_buffer);
+	g_test_add_func ("/Buffer/get-context-classes", test_get_context_classes);
+	g_test_add_func ("/Buffer/change-case", test_change_case);
+	g_test_add_func ("/Buffer/join-lines", test_join_lines);
+
+	return g_test_run();
+}
---
+++ ext/gtksourceview/tests/test-language.c
@@ -0,0 +1,146 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <gtksourceview/gtksource.h>
+
+typedef struct _TestFixture TestFixture;
+
+struct _TestFixture {
+	GtkSourceLanguageManager *manager;
+};
+
+/* If we are running from the source dir (e.g. during make check)
+ * we override the path to read from the data dir
+ */
+static void
+test_fixture_setup (TestFixture   *fixture,
+                    gconstpointer  data)
+{
+	gchar *dir;
+	gchar **lang_dirs;
+
+	dir = g_build_filename (TOP_SRCDIR, "data", "language-specs", NULL);
+
+	fixture->manager = gtk_source_language_manager_get_default ();
+
+	if (g_file_test (dir, G_FILE_TEST_IS_DIR))
+	{
+		lang_dirs = g_new0 (gchar *, 3);
+		lang_dirs[0] = dir;
+		lang_dirs[1] = g_test_build_filename (G_TEST_DIST, "language-specs", NULL);
+	}
+	else
+	{
+		const gchar * const *current;
+		int i;
+
+		g_free (dir);
+
+		current = gtk_source_language_manager_get_search_path (fixture->manager);
+		lang_dirs = g_new0 (gchar *, g_strv_length ((gchar **)current) + 2);
+		for (i = 0; current[i] != NULL; i++)
+		{
+			lang_dirs[i] = g_strdup(current[i]);
+		}
+		lang_dirs[i] = g_test_build_filename (G_TEST_DIST, "language-specs", NULL);
+	}
+
+	gtk_source_language_manager_set_search_path (fixture->manager, lang_dirs);
+	g_strfreev (lang_dirs);
+}
+
+static void
+test_fixture_teardown (TestFixture   *fixture,
+                       gconstpointer  data)
+{
+}
+
+static void
+compare_strv (gchar **strv,
+              gchar **expected_strv)
+{
+	if (expected_strv != NULL)
+	{
+		guint n, i;
+
+		n = g_strv_length (expected_strv);
+		for (i = 0; i < n; i++)
+		{
+			g_assert_cmpstr (strv[i], ==, expected_strv[i]);
+		}
+	}
+	else
+	{
+		g_assert (strv == NULL);
+	}
+}
+
+static void
+check_language (GtkSourceLanguage  *language,
+                const char         *id,
+                const char         *expected_name,
+                const char         *expected_section,
+                gboolean            expected_hidden,
+                const char         *expected_extra_meta,
+                gchar             **expected_mime,
+                gchar             **expected_glob,
+                gchar             **expected_styles,
+                const char         *style_id,
+                const char         *expected_style_name)
+{
+	gchar **mime;
+	gchar **glob;
+	gchar **styles;
+
+	g_assert_cmpstr (gtk_source_language_get_id (language), ==, id);
+	g_assert_cmpstr (gtk_source_language_get_name (language), ==, expected_name);
+	g_assert_cmpstr (gtk_source_language_get_section (language), ==, expected_section);
+	g_assert (gtk_source_language_get_hidden (language) == expected_hidden);
+	g_assert_cmpstr (gtk_source_language_get_metadata (language, "extra-meta"), ==, expected_extra_meta);
+
+	mime = gtk_source_language_get_mime_types (language);
+	compare_strv (mime, expected_mime);
+	g_strfreev (mime);
+
+	glob = gtk_source_language_get_globs (language);
+	compare_strv (glob, expected_glob);
+	g_strfreev (glob);
+
+	styles = gtk_source_language_get_style_ids (language);
+	compare_strv (styles, expected_styles);
+	g_strfreev (styles);
+
+	if (expected_style_name != NULL)
+	{
+		g_assert_cmpstr (gtk_source_language_get_style_name (language, style_id), ==, expected_style_name);
+	}
+}
+
+static void
+test_language (TestFixture   *fixture,
+               gconstpointer  data)
+{
+	GtkSourceLanguage *language;
+
+	language = gtk_source_language_manager_get_language (fixture->manager, "test-full");
+	gchar *mime[] = { "text/x-test", "application/x-test", NULL};
+	gchar *glob[] = { "*.test", "*.tst", NULL};
+	gchar *styles[] = { "test-full:keyword", "test-full:string", NULL};
+	check_language (language, "test-full", "Test Full", "Sources", FALSE, "extra", mime, glob, styles, "test-full:string", "String");
+
+	language = gtk_source_language_manager_get_language (fixture->manager, "test-empty");
+	check_language (language, "test-empty", "Test Empty", "Others", TRUE, NULL, NULL, NULL, NULL, NULL, NULL);
+}
+
+int
+main (int argc, char** argv)
+{
+	gtk_test_init (&argc, &argv);
+
+	g_test_add ("/Language/language-properties", TestFixture, NULL, test_fixture_setup, test_language, test_fixture_teardown);
+
+	return g_test_run();
+}
