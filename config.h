ef0892f5b8fb8c3d644cdd7b5e45d4180137ac22
paul.madore <phm@woodcoin.org>
2016-03-01 00:28:00 -0600

    -

--- config.h
+++ config.h
@@ -1,6 +1,3 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
 /* Define for debugging support */
 /* #undef DEBUG */
 
